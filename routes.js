f2b6dc7657717321575d081c1a0b1d94c8823aa0
David Christie <davidchristie.nz@gmail.com>
2016-12-08 21:52:18 +1300

    Implement posting comments

--- routes.js
+++ routes.js
@@ -1,7 +1,22 @@
-function getIndex(request, response) {
-  response.send('<h1>Index<h1>')
+ const format = require('date-format')
+
+const comments = []
+
+function getIndex (request, response) {
+  response.render('index', {comments})
+}
+
+function postIndex (request, response) {
+  // console.log(request.body)
+  comments.push({
+    name: request.body.name,
+    text: request.body.text,
+    time: format('hh:mm dd/MM/yyyy', new Date())
+  })
+  response.redirect('/')
 }
 
 module.exports = {
-  getIndex
+  getIndex,
+  postIndex
 }
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const hbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const routes = require('./routes')
 
 const server = express()
 
+// Middleware
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+server.set('view engine', 'hbs');
+
+server.use(express.static('public'))
+server.use(bodyParser.urlencoded({ extended: true }))
+
 // Routes
 server.get('/', routes.getIndex)
+server.post('/', routes.postIndex)
 
 module.exports = server
